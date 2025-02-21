8b17364b3e2585ecd079bd5ab2059f60a13d1d45
abhishekanand-02 <bgsabhishekanand02@gmail.com>
2025-02-21 12:13:59 +0530

    updated ci/cd

--- app.py
+++ app.py
@@ -5,22 +5,23 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        name = request.form.get('nm')  
-        return f"<center>Welcome <b>{name}</b>! 😊</center>" 
+        name = request.form.get('nm')  # Get the name from the form
+        return f"<center>Welcome <b>{name}</b>! 😊</center>"  # Show a greeting with the name
     else:
         return '''
         <html>
             <body>
-            <center>
-                <form method="POST">
-                    <p>Please Enter Your Name: </p>
-                    <p><input type="text" name="nm" /></p>
-                    <p><input type="submit" value="Submit" /></p>
-                </form>
+                <center>
+                    <form method="POST">
+                        <p>Please Enter Your Name:</p>
+                        <p><input type="text" name="nm" /></p>
+                        <p><input type="submit" value="Submit" /></p>
+                    </form>
+                </center>
             </body>
-            </center>
         </html>
         '''
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",port=5000)
+    # Start the Flask app on 0.0.0.0 to make it accessible externally on port 5000
+    app.run(host="0.0.0.0", port=5000)
